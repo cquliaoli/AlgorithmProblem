63cdacf14db26dbfe3a860ea2a96f3adb802f8e3
liaoli <956168488@qq.com>
2019-02-13 09:21:26 +0800

    正向推导，比较复杂

--- src/main/java/com/ll/DungeonGame.java
+++ src/main/java/com/ll/DungeonGame.java
@@ -9,7 +9,15 @@ import java.util.List;
  * time: 19:36
  */
 public class DungeonGame {
-
+    public static void main(String[] args) {
+        DungeonGame dungeonGame = new DungeonGame();
+        int[][] it = new int[][]{{-2,-3,3},{-5,-10,1},{10,30,-5}};
+        int[][] it1 = new int[][]{{1,-3,3},{0,-2,0},{-3,-3,-3}};//[[1,-3,3],[0,-2,0],[-3,-3,-3]]
+        int[][] dd = new int[][]{{0,0}};
+        System.out.println(dungeonGame.calculateMinimumHP(it));;
+        System.out.println(dungeonGame.calculateMinimumHP(dd));;
+        System.out.println(dungeonGame.calculateMinimumHP(it1));;
+    }
     class Pair{
         int cost;
         int remain;
@@ -21,22 +29,6 @@ public class DungeonGame {
         public Pair(Pair p){
             cost = p.cost;
             remain = p.remain;
-        }
-
-        public int getCost() {
-            return cost;
-        }
-
-        public void setCost(int cost) {
-            this.cost = cost;
-        }
-
-        public int getRemain() {
-            return remain;
-        }
-
-        public void setRemain(int remain) {
-            this.remain = remain;
         }
 
         public Pair() {
@@ -51,15 +43,24 @@ public class DungeonGame {
             history = new Pair();
             future = new Pair();
         }
+        public State(int cost){
+            this();
+            if(cost < 0){
+                future.cost = history.cost = -cost;
+                future.remain = history.remain  = 0;
+            }else {
+                future.cost = history.cost = 0;
+                future.remain = history.remain  = cost;
+            }
+        }
         public State(Pair history,Pair future) {
             this.history = history;
             this.future = future;
         }
         State(State pre, int cost){
-            history = new Pair();
-            future = new Pair();
-            future.remain = history.remain = pre.getHistory().remain + cost;
-            future.cost = history.cost = pre.getHistory().cost;
+            this();
+            future.remain = history.remain = pre.history.remain + cost;
+            future.cost = history.cost = pre.history.cost;
             if(history.remain < 0){
                 future.cost = history.cost += -history.remain;
                 future.remain = history.remain = 0;
@@ -107,69 +108,23 @@ public class DungeonGame {
             }
             return  new State(h, f);
         }
-
-        public Pair getHistory() {
-            return history;
-        }
-
-        public void setHistory(Pair history) {
-            this.history = history;
-        }
-
-        public Pair getFuture() {
-            return future;
-        }
-
-        public void setFuture(Pair future) {
-            this.future = future;
-        }
     }
-    public static void main(String[] args) {
-        DungeonGame dungeonGame = new DungeonGame();
-        int[][] it = new int[][]{{-2,-3,3},{-5,-10,1},{10,30,-5}};
-        int[][] it1 = new int[][]{{1,-3,3},{0,-2,0},{-3,-3,-3}};//[[1,-3,3],[0,-2,0],[-3,-3,-3]]
-        int[][] dd = new int[][]{{0,0}};
-        System.out.println(dungeonGame.calculateMinimumHP(it));;
-        System.out.println(dungeonGame.calculateMinimumHP(dd));;
-        System.out.println(dungeonGame.calculateMinimumHP(it1));;
-    }
+
     public int calculateMinimumHP(int[][] dungeon) {
         int row = dungeon.length;
         int col = dungeon[0].length;
-        // cost[i][j] 为从左上角到达[i,j] 的花费
-        // f[i][j] 为剩余的能量
-        //
+        // history 为从左上角到达[i,j] 的最小花费
+        // future 为剩余max(hp-cost)，即未来潜力最大值
+        // 新的state由左边和上面state更新
         State[][] f = new State[row][col];
         f[0][0] = new State();
-        if (dungeon[0][0] < 0){
-
-            f[0][0].future.cost =f[0][0].history.cost = -dungeon[0][0];
-            f[0][0].future.remain =f[0][0].history.remain  = 0;
-        }else {
-            f[0][0].future.cost =f[0][0].history.cost = 0;
-            f[0][0].future.remain =f[0][0].history.remain  = dungeon[0][0];
-        }
+        f[0][0] = new State(dungeon[0][0]);
         int i, j;
         for (j = 1; j < col; j++){
             f[0][j]= new State(f[0][j - 1], dungeon[0][j]);
-
-            /*f[0][j].history.remain = f[0][j - 1].history.remain + dungeon[0][j];
-            f[0][j].history.cost = f[0][j - 1].history.cost;
-            if(f[0][j].history.remain < 0){
-                f[0][j].history.cost +=  -f[0][j].history.remain;
-                f[0][j].history.remain = 0;
-            }
-            f[0][j].future = new Pair(f[0][j].history);*/
         }
         for (i = 1; i < row; i++){
             f[i][0]= new State(f[i - 1][0], dungeon[i][0]);
-            /*f[i][0].history.remain = f[i - 1][0].history.remain + dungeon[i][0];
-            f[i][0].history.cost = f[i - 1][0].history.cost;
-            if(f[i][0].history.remain < 0){
-                f[i][0].history.cost += - f[i][0].history.remain;
-                f[i][0].history.remain = 0;
-            }
-            f[i][0].future = new Pair(f[i][0].history);*/
         }
 
         for (i = 1; i < row; i++){
